2c1642e7963ee3f3f3921837ebab674fd02e8b9a
Javier Contreras <javierin_kl@hotmail.com>
2018-08-17 07:48:57 -0700

    Agregar archivos de proyecto.

---
+++ metodosEstaticos/matematicas.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metodosEstaticos
+{
+    class matematicas
+    {
+        public static int Potencia(int numeroBase, int exponente)
+        {
+            int resultado = 1;
+            if (exponente != 0)
+            {
+                for (int i = 0; i < exponente; i++)
+                {
+                    resultado *= numeroBase;
+                }
+            }
+            return resultado;
+        }
+    }
+}
